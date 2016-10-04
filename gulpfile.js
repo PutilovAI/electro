0169f8211801ed020d889a0a3eafd70b9927842c
aleksandrputilov <aleksandrputilov@yandexru>
2016-10-05 02:48:50 +0500

    second

--- gulpfile.js
+++ gulpfile.js
@@ -11,17 +11,20 @@ const cssnano = require('gulp-cssnano');
 const autoprefixer = require('gulp-autoprefixer');
 const del = require('del');
 const prettify = require('gulp-html-prettify');
-var browserSync  = require('browser-sync');
+const browserSync  = require('browser-sync');
+const flatten  = require('gulp-flatten');
 
 gulp.task('html', function() {
 	return gulp.src('./src/templates/pages/*.*')
 		.pipe(pug())
 		.pipe(prettify({indent_size: 4}) )
-		.pipe(gulp.dest('./dist'));
+		.pipe(gulp.dest('./dist'))
+		.pipe( browserSync.reload({stream: true}) ) ;
+
 })
 
 gulp.task('style', function() {
-	return gulp.src('./src/assets/style/*.*')
+	return gulp.src('./src/assets/style/main.styl')
 		.pipe(stylus())
 		.pipe(autoprefixer(
 			['last 15 versions'], { cascade: true })
@@ -38,7 +41,16 @@ gulp.task('js', function() {
 		])
 		.pipe( concat('main.js') )
 		//.pipe( uglifyjs() )
-		.pipe( gulp.dest('./dist/assets/js') );
+		.pipe( gulp.dest('./dist/assets/js') )
+})
+
+gulp.task('img', function() {
+	return gulp.src([
+			'./src/blocks/**/*.+(png|jpg|jpeg|gif|svg)',
+			'./src/assets/images/**/*.+(png|jpg|jpeg|gif|svg)'
+		])
+		.pipe(flatten({ includeParents: 0 }))
+		.pipe( gulp.dest('./dist/assets/images') );
 })
 
 gulp.task('fonts', function() {
@@ -55,16 +67,17 @@ gulp.task('connect', function() { // Создаем таск browser-sync
 	});
 });
 
+gulp.task('reload', function(){
+	return browserSync.reload
+})
 gulp.task('watch', function() {
 	gulp.watch(['./src/assets/style/*.*', './src/blocks/**/*.styl'], ['style']);
-	gulp.watch([
-		'./src/blocks/**/*.pug',
-		'./src/templates/**/*.pug'], browserSync.reload);
-	gulp.watch([
-			'./src/blocks/**/*.js',
-			'./src/assets/js/*.js'], browserSync.reload);
+	gulp.watch(['./src/blocks/**/*.pug','./src/templates/**/*.pug'],  ['html'] );
+	gulp.watch(['./src/blocks/**/*.js',	'./src/assets/js/*.js'], ['js']);
+
+
 });
 
-gulp.task('default', ['style', 'js', 'html', 'fonts']);
+gulp.task('default', ['style', 'js', 'html', 'fonts', 'img']);
 
 gulp.task('dev', ['default', 'connect', 'watch']);
